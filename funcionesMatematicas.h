3e8038f71e6f01b8d8ac28aa57f01a745542be11
Ramiro Machado <machado.ramiromiguel@gmail.com>
2013-04-12 17:18:06 -0300

    La funcion resta testeada y funcionando

--- funcionesMatematicas.h
+++ funcionesMatematicas.h
@@ -1,10 +1,22 @@
 /* Este archivo contiene todo lo referente a la funcionalidad matematica necesaria */
 
 numComplejo suma(numComplejo, numComplejo);
+numComplejo resta(numComplejo, numComplejo);
 
+
+// La funcion suma dos numComplejos binomicos y devuelve un numComplejo binomico
 numComplejo suma(numComplejo primerOperando, numComplejo segundoOperando){
 
             primerOperando.primerElemento += segundoOperando.primerElemento;
             primerOperando.segundoElemento += segundoOperando.segundoElemento;
             return primerOperando;
 }
+
+// La funcion resta dos numComplejos binomicos
+numComplejo resta(numComplejo primerOperando, numComplejo segundoOperando){
+
+            primerOperando.primerElemento -= segundoOperando.primerElemento;
+            primerOperando.segundoElemento -= segundoOperando.segundoElemento;
+            return primerOperando;
+}
+
