4090bf76048a97322529754bd0027b3b40ed7300
Ramiro Machado <machado.ramiromiguel@gmail.com>
2013-04-12 15:33:30 -0300

    Transformaciones de binomica a polar y viceversa funcionando

--- tratamiendoDeDatos.h
+++ tratamiendoDeDatos.h
@@ -10,3 +10,24 @@ typedef struct numComplejo{
         float segundoElemento;          //Valor imaginario en binomica o argumento en polar
         short int tipo;                 //Permite identificar con un define el tipo de representacion que es
 } numComplejo;
+
+numComplejo binomicaAPolar(numComplejo);
+numComplejo polarABinomica(numComplejo);
+
+// La funcion transforma un numComplejo de binomica a polar (debe recibir un numComplejo en binomica)
+numComplejo binomicaAPolar(numComplejo numeroBinomica){
+            numComplejo polar;
+            polar.primerElemento = sqrt(pow(numeroBinomica.primerElemento,2)+ pow(numeroBinomica.segundoElemento,2));
+            polar.segundoElemento = atan2(numeroBinomica.segundoElemento, numeroBinomica.primerElemento); // TODO CORREGIR EL ARGUMENTO
+            polar.tipo = POLAR;
+            return polar;
+}
+
+// La funcion transforma un numComplejo de polar a binomica (debe recibir un numComplejo en polar)
+numComplejo polarABinomica(numComplejo numeroPolar){
+            numComplejo binomica;
+            binomica.primerElemento = cos(numeroPolar.segundoElemento) * numeroPolar.primerElemento;
+            binomica.segundoElemento = sin(numeroPolar.segundoElemento) * numeroPolar.primerElemento;
+            binomica.tipo = BINOMICA;
+            return binomica;
+}
