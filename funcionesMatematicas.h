9fb7dad1ca77b17b3e28e7d557bd0d011f74dbdd
Ramiro Machado <machado.ramiromiguel@gmail.com>
2013-04-14 23:36:53 -0400

    Mergeo los branches

--- funcionesMatematicas.h
+++ funcionesMatematicas.h
@@ -1,45 +1,66 @@
 /* Este archivo contiene todo lo referente a la funcionalidad matematica necesaria */
 
+#define BINOMICA 1
+#define POLAR 2
+
 numComplejo suma(numComplejo, numComplejo);
 numComplejo resta(numComplejo, numComplejo);
 numComplejo multiplicacion(numComplejo, numComplejo);
-numComplejo divicion(numComplejo, numComplejo);
+numComplejo division(numComplejo, numComplejo);
 
 
 // La funcion suma dos numComplejos binomicos y devuelve un numComplejo binomico
-numComplejo suma(numComplejo primerOperando, numComplejo segundoOperando){
-
-            primerOperando.primerElemento += segundoOperando.primerElemento;
-            primerOperando.segundoElemento += segundoOperando.segundoElemento;
-            return primerOperando;
+numComplejo suma(numComplejo primerOperando, numComplejo segundoOperando) {
+            if (primerOperando.tipo == BINOMICA && segundoOperando == BINOMICA) {
+                //TODO: Si no se cumple la condicion, transformar los operandos a la forma binomica
+                //para poder operar con cualquier tipo de complejo
+                primerOperando.primerElemento += segundoOperando.primerElemento;
+                primerOperando.segundoElemento += segundoOperando.segundoElemento;
+                return primerOperando;
+            } else {
+                printf("Ambos operandos deben estar en forma binomica");
+            }
 }
 
 // La funcion resta dos numComplejos binomicos
-numComplejo resta(numComplejo primerOperando, numComplejo segundoOperando){
-
-            primerOperando.primerElemento -= segundoOperando.primerElemento;
-            primerOperando.segundoElemento -= segundoOperando.segundoElemento;
-            return primerOperando;
+numComplejo resta(numComplejo primerOperando, numComplejo segundoOperando) {
+            if (primerOperando.tipo == BINOMICA && segundoOperando == BINOMICA) {
+                //TODO: Si no se cumple la condicion, transformar los operandos a la forma binomica
+                //para poder operar con cualquier tipo de complejo
+                primerOperando.primerElemento -= segundoOperando.primerElemento;
+                primerOperando.segundoElemento -= segundoOperando.segundoElemento;
+                return primerOperando;
+            } else {
+                printf("Ambos operandos deben estar en forma binomica");
+            }
 }
 
 // La funcion multiplica dos numCOmplejos polares
-numComplejo multiplicacion(numComplejo primerOperando, numComplejo segundoOperando){
-            
-            primerOperando.primerElemento *= segundoOperando.primerElemento;
-            primerOperando.segundoElemento += segundoOperando.segundoElemento;
-            return primerOperando;
+numComplejo multiplicacion(numComplejo primerOperando, numComplejo segundoOperando) {
+            if (primerOperando.tipo == POLAR && segundoOperando == POLAR) {  
+                //TODO: Si no se cumple la condicion, transformar los operandos a la forma polar
+                //para poder operar con cualquier tipo de complejo
+                primerOperando.primerElemento *= segundoOperando.primerElemento;
+                primerOperando.segundoElemento += segundoOperando.segundoElemento;
+                return primerOperando;
+            } else {
+                printf("Ambos operandos deben estar en forma polar");
+            }
 }
 
 // La funcion divide dos numComplejos polares
-numComplejo divicion(numComplejo primerOperando, numComplejo segundoOperando){
-            
-            if( segundoOperando.segundoElemento != 0){ 
-                primerOperando.primerElemento /= segundoOperando.primerElemento;
-                primerOperando.segundoElemento -= segundoOperando.segundoElemento;
-            } else
-              printf("No se puede dividir por 0, se devuelve el primer operando.\n");   //TODO ALGO MEJOR
-            
-            return primerOperando;           
+numComplejo division(numComplejo primerOperando, numComplejo segundoOperando) {
+            if (primerOperando.tipo == POLAR && segundoOperando == POLAR) {
+                //TODO: Si no se cumple la condicion, transformar los operandos a la forma polar
+                //para poder operar con cualquier tipo de complejo
+                if (segundoOperando.primerElemento != 0) { 
+                    primerOperando.primerElemento /= segundoOperando.primerElemento;
+                    primerOperando.segundoElemento -= segundoOperando.segundoElemento;
+                } else {
+                    printf("No se puede dividir por 0, se devuelve el primer operando.\n");   //TODO ALGO MEJOR
+                    return primerOperando;
+                }
+            } else {
+                printf("Ambos operandos deben estar en forma polar");
+            }
 }
-
-
--- tratamiendoDeDatos.h
+++ tratamiendoDeDatos.h
@@ -1,6 +1,6 @@
 /* Este archivo contiene todo lo referente al tratamiento de los datos, es decir
 estructuras para almacenar los numeros, conversiones entre formas, etc */
-#include <math.h> 
+#include <math.h>
 
 #define BINOMICA 1
 #define POLAR 2
@@ -15,12 +15,15 @@ numComplejo binomicaAPolar(numComplejo);
 numComplejo polarABinomica(numComplejo);
 
 // La funcion transforma un numComplejo de binomica a polar (debe recibir un numComplejo en binomica)
+
 numComplejo binomicaAPolar(numComplejo numeroBinomica){
             if(numeroBinomica.tipo == BINOMICA){
                numComplejo polar;
                polar.primerElemento = sqrt(pow(numeroBinomica.primerElemento,2)+ pow(numeroBinomica.segundoElemento,2));
                polar.segundoElemento = atan2(numeroBinomica.segundoElemento, numeroBinomica.primerElemento); // TODO CORREGIR EL ARGUMENTO
                polar.tipo = POLAR;
+            } else {
+                printf("El complejo debe estar en forma binomica");
             }
             return polar;
 }
@@ -28,10 +31,12 @@ numComplejo binomicaAPolar(numComplejo numeroBinomica){
 // La funcion transforma un numComplejo de polar a binomica (debe recibir un numComplejo en polar)
 numComplejo polarABinomica(numComplejo numeroPolar){
             if(numeroPolar.tipo == POLAR){
-                numComplejo binomica;
-                binomica.primerElemento = cos(numeroPolar.segundoElemento) * numeroPolar.primerElemento;
-                binomica.segundoElemento = sin(numeroPolar.segundoElemento) * numeroPolar.primerElemento;
-                binomica.tipo = BINOMICA;
+                numComplejo polar;
+                polar.primerElemento = sqrt(pow(numeroBinomica.primerElemento,2) + pow(numeroBinomica.segundoElemento,2));
+                polar.segundoElemento = atan2(numeroBinomica.segundoElemento, numeroBinomica.primerElemento); // TODO CORREGIR EL ARGUMENTO
+                polar.tipo = POLAR;
+            } else {
+                printf("El complejo debe estar en forma binomica");
             }
-            return binomica;
+            return polar;
 }
